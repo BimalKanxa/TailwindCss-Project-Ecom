e361ac6b46f7388e7833f012267ce1da19d5b50c
BimalKanxa <sharmabimal2003@gmail.com>
2024-04-14 19:53:26 +0530

    all code push

---
+++ src/sections/Services.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {services} from '../constants'
+import ServiceCard from '../components/ServiceCard'
+import ScrollAnimation from 'react-animate-on-scroll';
+
+const Services = () => {
+  return (
+    <ScrollAnimation animateIn="fadeIn">
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      
+        {services.map((service) => (
+          <ServiceCard key={service.label} {...service}/>
+        ))}
+
+    </section>
+    </ScrollAnimation>
+  )
+}
+
+export default Services
